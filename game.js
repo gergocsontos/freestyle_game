a1f58bb573561dcdca7eef3f257c799c5dd91d99
Vincze Gábor <cantataprofana1930@gmail.com>
2021-06-21 18:21:56 +0200

    put the card-slots onto the page

--- game.js
+++ game.js
@@ -5,7 +5,6 @@ function initGame() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const pairs = parseInt(urlParams.get('cards'));
-    let chosenNumbers = [5, 10, 13, 4];
 // element.style.src = `images/${i}.png`
     const maxNum = 10;
     const container = document.querySelector('.container')
@@ -13,6 +12,7 @@ function initGame() {
     function getNumbers(number) {
         let imageNumbers = [];
         do {
+            // debugger;
             const randomNumber = Math.ceil(Math.random() * maxNum);
             console.log(randomNumber);
             if (!imageNumbers.includes(randomNumber)) {
@@ -20,16 +20,28 @@ function initGame() {
             }
         }
         while (imageNumbers.length !== number);
-        console.log(imageNumbers);
+        imageNumbers.push.apply(imageNumbers, imageNumbers)
+        imageNumbers.sort((a, b) => 0.5 - Math.random());
+        imageNumbers.sort((a, b) => 0.5 - Math.random());
+        console.log(imageNumbers)
+        console.log(typeof(imageNumbers))
         return imageNumbers;
     }
     divPlacer(pairs)
     function divPlacer(pairs) {
+        let imageIds = getNumbers(pairs)
+        console.log(imageIds)
         pairs *= 2;
         for (let i = 0; i < pairs; i++) {
+
             const newDiv = document.createElement('div');
             newDiv.classList.add('closed');
             newDiv.classList.add('card-slot');
+            const img = document.createElement('img')
+            img.src = `images/${imageIds[i]}.png`
+            img.dataset.id = imageIds[i]
+            newDiv.appendChild(img)
+
             container.appendChild(newDiv);
         }
     }
