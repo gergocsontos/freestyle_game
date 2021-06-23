ecfd59945b6c22157d66f750a39735c22cbbf3d2
Vincze Gábor <cantataprofana1930@gmail.com>
2021-06-23 16:18:07 +0200

    create remaining user stories
    except high score

--- game.js
+++ game.js
@@ -2,14 +2,19 @@ let game = {
     slot: document.querySelector(".card-slot"),
     isClickable: true,
     pairsVar: undefined,
+    turnLimit: undefined,
+    timeLimit: undefined,
     pairs: function () {
         let queryString = window.location.search;
         let urlParams = new URLSearchParams(queryString);
         game.pairsVar = parseInt(urlParams.get('cards'));
+        game.turnLimit = urlParams.get('turns');
+        game.timeLimit = urlParams.get('timer');
     },
     openCounter: 0,
-    maxNum: 10,
+    maxNum: 30,
     foundPairs: 0,
+    // startTime - now() - convert to secs
     clock: 0,
     hasStarted: false,
     time: undefined,
@@ -20,18 +25,24 @@ let game = {
         clearInterval(game.time)
         const postgameText = document.querySelector('#postgame-text');
         postgameText.textContent = 'Nice job!';
+        document.querySelector('#play-again').hidden = false;
+        // reload page ??
+        // location.reload();
     },
     initGame: function () {
         game.pairs();
-        console.log(game.pairsVar);
+        game.timerCalculator();
+        game.turnCalculator();
         game.divPlacer();
+        game.displayTurns();
+        game.displayTimer();
+        console.log(game.timeLimit, game.turnLimit)
     }
 
     ,
     getNumbers: function () {
         let imageNumbers = [];
         do {
-            // debugger;
             const randomNumber = Math.ceil(Math.random() * game.maxNum);
             if (!imageNumbers.includes(randomNumber)) {
                 imageNumbers.push(randomNumber);
@@ -54,7 +65,8 @@ let game = {
             newDiv.classList.add('closed');
             newDiv.classList.add('card-slot');
             const img = document.createElement('img')
-            img.src = `images/${imageIds[i]}.png`
+            img.src = `images/${imageIds[i]}.png`;
+            img.draggable = false;
             img.style.opacity = '0';
             img.dataset.id = imageIds[i]
             // let openCounter = 0
@@ -70,7 +82,9 @@ let game = {
                         if (game.openCounter === 2) {
                             game.isClickable = false;
                             game.turnMeter += 1;
-                            document.querySelector('#turns').value = game.turnMeter;
+                            ///////////
+                            game.displayTurns();
+
                             game.openCounter = 0;
                             e.target.style.opacity = '1';
                             if (e.target.dataset.id === game.firstPic.dataset.id) {
@@ -86,8 +100,11 @@ let game = {
                                     game.isClickable = true;
                                     game.firstPic.style.opacity = '0';
                                     e.target.style.opacity = '0';
-                                }, 1500);
+                                }, 1000);
 
+                            }
+                            if (game.turnLimit === game.turnMeter) {
+                                game.lostGame();
                             }
                         } else {
                             e.target.style.opacity = '1';
@@ -104,14 +121,58 @@ let game = {
         clearInterval(game.time)
         const postgameText = document.querySelector('#postgame-text');
         postgameText.textContent = 'Boo, you lost!'
+        document.querySelector('#play-again').hidden = false;
     },
     clockStart: function () {
         if (!game.hasStarted) {
+            game.hasStarted = true;
             game.time = setInterval(() => {
-                    game.hasStarted = true;
-                    game.clock++;
-                    document.querySelector('#time').value = game.clock;
-                }, 1000)
+                game.clock++;
+                document.querySelector('#time').value = game.clock;
+                document.querySelector('#timer').value = game.timeLimit - game.clock;
+                if (game.timeLimit - game.clock === 0) {
+                    game.lostGame();
+                }
+            }, 1000)
+        }
+    },
+    turnCalculator: function () {
+        if (game.turnLimit === 'hard') {
+            Math.ceil(game.turnLimit = game.pairsVar * 1.5);
+        } else if (game.turnLimit === 'medium') {
+            game.turnLimit = game.pairsVar * 2;
+        } else if (game.turnLimit === 'easy') {
+            game.turnLimit = game.pairsVar * 3;
+        }
+    },
+    timerCalculator: function () {
+        if (game.timeLimit === 'hard') {
+            game.timeLimit = game.pairsVar * 6;
+        } else if (game.timeLimit === 'medium') {
+            game.timeLimit = game.pairsVar * 9;
+        } else if (game.timeLimit === 'easy') {
+            game.timeLimit = game.pairsVar * 12;
+        }
+    },
+    displayTurns: function () {
+        if (Number.isInteger(game.turnLimit)) {
+            console.log(`${game.turnLimit} / ${game.turnMeter}`)
+            document.querySelector('#turns').value = `${game.turnLimit} / ${game.turnMeter}`
+        } else {
+            document.querySelector('#turns').value = game.turnMeter;
+        }
+    },
+    displayTimer: function () {
+        let timeInputDiv = document.querySelector('.time');
+        let timerInputDiv = document.querySelector('.timer');
+        let timerInput = document.querySelector('#timer');
+        let timeInput = document.querySelector('#time');
+        if (Number.isInteger(game.timeLimit)) {
+            timeInputDiv.style.display = 'none';
+            timerInput.value = game.timeLimit - game.clock;
+        } else {
+            timeInput.value = game.clock;
+            timerInputDiv.style.display = 'none';
         }
     }
 
